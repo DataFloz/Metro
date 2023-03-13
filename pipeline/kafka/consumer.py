27236ad62161abb419c5eef2fd4f5dd17d4c950b
ofirelarat <96ofir11@gmail.com>
2023-03-13 21:57:47 +0200

    use classes

--- pipeline/kafka/consumer.py
+++ pipeline/kafka/consumer.py
@@ -1,38 +1,38 @@
 from confluent_kafka import Consumer, KafkaError, KafkaException
 
-conf = {
+class kafkaConsumer:
+    def __init__(self):
+        conf = {
         'bootstrap.servers': "host1:9092,host2:9092",
         'group.id': "test",
         'auto.offset.reset': 'smallest'
-    }
+        }
 
-consumer = Consumer(conf)
+        self.consumer = Consumer(conf)
 
 
-running = True
+    def consume(self, topics):
+        self.running = True
+        try:
+            self.consumer.subscribe(topics)
 
-def consume(topics):
-    try:
-        consumer.subscribe(topics)
+            while self.running:
+                msg = self.consumer.poll(timeout=1.0)
+                if msg is None: continue
 
-        while running:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None: continue
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        # End of partition event
+                        # Will remove later
+                        print('%% %s [%d] reached end at offset %d\n' %
+                                        (msg.topic(), msg.partition(), msg.offset()))
+                    elif msg.error():
+                        raise KafkaException(msg.error())
+                else:
+                    # msg_process(msg)
+                    print("some msg pocess")
+        finally:
+            self.consumer.close()
 
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
-                    # End of partition event
-                    # Will remove later
-                    print('%% %s [%d] reached end at offset %d\n' %
-                                     (msg.topic(), msg.partition(), msg.offset()))
-                elif msg.error():
-                    raise KafkaException(msg.error())
-            else:
-                # msg_process(msg)
-                print("some msg pocess")
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
-
-def shutdown():
-    running = False
+    def shutdown(self):
+        self.running = False
