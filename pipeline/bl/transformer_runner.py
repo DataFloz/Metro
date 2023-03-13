f99e235d68cb2ab859e8c3888ea10dccff6911c7
ofirelarat <96ofir11@gmail.com>
2023-03-13 22:08:38 +0200

    added transformer

---
+++ pipeline/bl/transformer_runner.py
@@ -0,0 +1,8 @@
+class Transformer():
+    def __init__():
+        print("here we will initiate the transformer with the pipeline config")
+
+    def run_logic(msg):
+        print("run the specific FAAS logic and return the result")
+        
+        return {}
