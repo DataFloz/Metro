2becec16ae114b77e69e578e85b17c0a44e8d334
Alon Goldberg <email>
2023-03-18 13:31:27 +0200

    fix

--- runner/main.py
+++ runner/main.py
@@ -5,30 +5,8 @@ from datetime import datetime
 # from pipeline.bl.transformer_runner import Transformer
 
 def main():
-    client = docker.from_env()
-    now = datetime.now()
-    
-    # print("now =", now)
+    print("start build pipelines")
+    pipelines_configuration = read_configuration_file()
 
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime('%F %T.%f')[:-3]
-    print("date and time =", dt_string)
-    # print("start build pipelines")
-    # pipelines_configuration = read_configuration_file()
-    client.containers.run("test")
-
-    now = datetime.now()
-    
-    # print("now =", now)
-
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime('%F %T.%f')[:-3]
-    print("date and time =", dt_string)
-
-    # for i in pipelines_configuration['pipelines']:
-    #     print(i)
-    #     # transformer = Transformer()   
-    # # print(pipelines_configuration['pipelines'][0]['transformation'])
-    # # print("run each pipeline with its configuration")
-
+    print('end build pipelines')
 main()
