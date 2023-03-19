b50e07fa9dc6727838532d6b53fb7a5262a52287
ofirelarat <96ofir11@gmail.com>
2023-03-19 23:55:46 +0200

    router for the runner

--- runner/main.py
+++ runner/main.py
@@ -1,13 +1,10 @@
+import api.router
 from configuration_reader import read_configuration_file
 from pipe_runner import run_pipeline
 
+
 def main():
-    print("start build pipelines")
-    pipelines_configuration = read_configuration_file()
+    print("runner api is up")
 
-    print("run each pipeline with its configuration")
-    for pipeline_config in pipelines_configuration:
-        run_pipeline(pipeline_config)
 
-    print('end build and run pipelines')
 main()
