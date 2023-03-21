dacd7fddbbc5fbf21f2d198f7b28e7372069482b
Alon Goldberg <email>
2023-03-22 00:23:40 +0200

    remove default content

---
+++ client/src/components/pipepline.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const Pipeline = () => {
+  return (
+    <>
+      <div>pipeline name</div>
+      <div>input</div>
+    </>
+  );
+};
+
+export default Pipeline;
