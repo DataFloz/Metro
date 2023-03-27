c4e3da487a744d46c5856979a650a50da4cc41eb
ofirelarat <96ofir11@gmail.com>
2023-03-27 23:39:09 +0300

    added kafka connector to pod and display connects

---
+++ client/src/components/connectors-table.tsx
@@ -0,0 +1,21 @@
+import { Connector } from '@/models/connector';
+import { Accordion } from '@mantine/core';
+
+interface TableProps {
+    connectors: Connector[];
+}
+
+export default function PipelinesTable({connectors}: TableProps) {
+    console.log(connectors)
+    const rows = connectors.map((row) => (
+        <Accordion.Item value="customization">
+            <Accordion.Control>Kafka Connector</Accordion.Control>
+            <Accordion.Panel>{row.name}, {row.brokers} | {row.group_id}</Accordion.Panel>
+        </Accordion.Item>
+      ));
+  
+  return ( 
+    <Accordion title='Connectors' variant="filled" radius="md"  chevronPosition="left" defaultValue="customization">
+      {rows}
+    </Accordion>)
+}
