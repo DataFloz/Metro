b2577a52d5e514a0f6cd2ea7295106ddf1eecb59
ofirelarat <96ofir11@gmail.com>
2023-04-21 18:17:57 +0300

    fix pylint

--- pipeline/bl/sql_transformer_runner.py
+++ pipeline/bl/sql_transformer_runner.py
@@ -1,21 +1,19 @@
 import sqlite3
 import pandas as pd
+from bl.transformer_runner_interface import TransforerRunnerInterface
 
 
-from bl.iTransformerRunnerInterface import iTransforerRunnerInterface
-
-
-class SQLTransformer(iTransforerRunnerInterface):
+class SQLTransformer(TransforerRunnerInterface):
     def __init__(self, query):
         self.query = query
 
     def run_logic(self, msg):
-        df = pd.DataFrame.from_dict(msg)
+        data_frame = pd.DataFrame.from_dict(msg)
         conn = sqlite3.connect(":memory:")
-        
+
         # msg is name the query will always use
-        df.to_sql('msg', conn, index=False)
-        
+        data_frame.to_sql('msg', conn, index=False)
+
         query_result = pd.read_sql_query(self.query, conn)
 
         return query_result
