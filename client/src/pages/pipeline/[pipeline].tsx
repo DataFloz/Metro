241b1ed358e0447943cbbe6db21a6ff53200906d
ofirelarat <96ofir11@gmail.com>
2023-07-29 22:31:56 +0300

    seperate compoenents

--- client/src/pages/pipeline/[pipeline].tsx
+++ client/src/pages/pipeline/[pipeline].tsx
@@ -1,12 +1,11 @@
 import { useDataContext } from '@/context/context';
 import { useRouter } from 'next/router';
-import { Pipeline } from '../../models/pipeline';
-import { Button, Drawer, Input, Table } from '@mantine/core';
+import { Button, Drawer, Table } from '@mantine/core';
 import { Grid } from '@mantine/core';
 import ProduceTest from '@/components/test-message-dialog';
 import { useDisclosure } from '@mantine/hooks';
-import useAxios from 'axios-hooks';
 import PipelineTransformMessages from '@/components/pipeline-transformed-messages';
+import PipelineMetadata from '@/components/pipeline-metadata';
 
 export default function Home() {
     const router = useRouter();
@@ -16,70 +15,21 @@ export default function Home() {
 
     const [messagesDrawerOpened, { open, close }] = useDisclosure(false);
 
-    const [{ data, loading, error }] = useAxios<any>({
-        url: '/api/pipeline-metadata',
-        method: 'post',
-        data: { pipeline: dataContext?.config.pipelines.filter((pipe) => pipe.name === pipeline)[0],
-            kafkaConnector: dataContext!.config.connector
-        }
-    });
-
     const onOpenMessagesDrawer = async () => {
         open();
-    };
-
-    const getInputValue = (key: string, value: string) => {
-        return (
-            <div style={{ width: '20%' }}>
-                <Input.Wrapper label={key} maw={320} mx="auto">
-                    <Input radius="lg" disabled value={value} />
-                </Input.Wrapper>
-            </div>
-        );
-    };
-    const iterate = (pipeline: Pipeline) => {
-        return Object.entries(pipeline).map(([key, value]) => {
-            console.log('key', key);
-            switch (key) {
-                case 'name':
-                    return getInputValue(key, value);
-                case 'input':
-                    return getInputValue('input topic', value.topic);
-                case 'output':
-                    return getInputValue('output topic', value.topic);
-                case 'transformation':
-                    switch (value.type) {
-                        case 'container':
-                            return getInputValue(
-                                'container url',
-                                value.container_url
-                            );
-                        case 'http':
-                            return getInputValue('HTTP url', value.http_url);
-                        case 'sql':
-                            return getInputValue('SQL query', value.sql_query);
-                        case 'pickle':
-                            return getInputValue(
-                                'pickle file name',
-                                value.file_name
-                            );
-                        default:
-                            break;
-                    }
-                default:
-                    break;
-            }
-        });
     };
 
     let currentPipeline = dataContext?.config.pipelines.filter(
         (pipe) => pipe.name === pipeline
     )[0];
+
     return (
         <>
             {currentPipeline ? (
                 <>
-                    {iterate(currentPipeline)}
+                    <PipelineMetadata 
+                        pipeline={currentPipeline}
+                        connector={dataContext!.config.connector} />
                     <Grid m={15}>
                         <Grid.Col span={2}>
                             <ProduceTest
@@ -94,7 +44,6 @@ export default function Home() {
                                 Show transfomed messages
                             </Button>
                         </Grid.Col>
-                        {data}
                     </Grid>
                     <Drawer
                         opened={messagesDrawerOpened}
